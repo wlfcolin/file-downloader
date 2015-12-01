de0a7444e5f7980b77d0ec6bf7c52c99449d9219
wlfcolin <wlfcolin@hotmail.com>
2015-12-01 18:36:57 +0800

    add some callback for advance operation

---
+++ FileDownloader/src/main/java/org/wlf/filedownloader/listener/OnSycnMoveDownloadFilesListener.java
@@ -0,0 +1,24 @@
+package org.wlf.filedownloader.listener;
+
+import org.wlf.filedownloader.DownloadFileInfo;
+
+import java.util.List;
+
+/**
+ * @author wlf(Andy)
+ * @datetime 2015-12-01 18:22 GMT+8
+ * @email 411086563@qq.com
+ */
+public abstract class OnSycnMoveDownloadFilesListener implements OnMoveDownloadFilesListener {
+
+    /**
+     * sync move(for example,the caller will need to do it's own database record)
+     *
+     * @param downloadFilesNeedMove download files needed to move
+     * @param downloadFilesMoved    download files moved
+     * @return true means the caller hopes to continue the operation,otherwise the caller may get in trouble itself,the file-downloader will rollback the operation
+     */
+    boolean onDoSyncMoveDownloadFiles(List<DownloadFileInfo> downloadFilesNeedMove, List<DownloadFileInfo> downloadFilesMoved) {
+        return true;
+    }
+}
---
+++ FileDownloader/src/main/java/org/wlf/filedownloader/listener/OnSyncDeleteDownloadFileListener.java
@@ -0,0 +1,21 @@
+package org.wlf.filedownloader.listener;
+
+import org.wlf.filedownloader.DownloadFileInfo;
+
+/**
+ * @author wlf(Andy)
+ * @datetime 2015-12-01 17:40 GMT+8
+ * @email 411086563@qq.com
+ */
+public abstract class OnSyncDeleteDownloadFileListener implements OnDeleteDownloadFileListener {
+
+    /**
+     * sync delete(for example,the caller will need to do it's own database record)
+     *
+     * @param downloadFileDeleted download file deleted
+     * @return true means the caller hopes to continue the operation,otherwise the caller may get in trouble itself,the file-downloader will rollback the operation
+     */
+    boolean onDoSyncDeleteDownloadFile(DownloadFileInfo downloadFileDeleted) {
+        return true;
+    }
+}
